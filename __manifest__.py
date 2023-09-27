db84cfa1a076c81654bad359d550527eff8f8908
rrawann <rawan.sb@yahoo.com>
2023-09-27 09:41:28 +0300

    init folder

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+
+{
+    'name': 'HR_System',
+    'version': '16.0.2.0.7',
+    'category': 'HR_System',
+    'summary': """HR_System.""",
+    'description': """
+                     HR_System
+                    """,
+    'author': 'yousen tech Techno Solutions, Odoo SA',
+    'website': "https://www.qimamhd.com",
+    'company': 'yousen Techno Solutions',
+    'maintainer': 'yousen Techno Solutions',
+    'depends': ['base','account','stock','report_xlsx'],
+    'data': [   
+                'security/ir.model.access.csv',
+               
+              
+         ],
+     
+    'license': 'LGPL-3',
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
