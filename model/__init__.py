2406a9b4e08d86ca2d80b41833a03494711a16a1
ashwagMohsen <ashwag94521@gmail.com>
2023-09-27 16:10:48 +0300

    add products and other details

--- model/__init__.py
+++ model/__init__.py
@@ -3,3 +3,5 @@ from .import off_days
 from . import hr_emplyee
 from . import hr_departments
 from . import hr_partners
+from . import hr_partners
+from . import products
--- model/hr_departments.py
+++ model/hr_departments.py
@@ -4,5 +4,6 @@ from odoo.exceptions import ValidationError
 class Departments(models.Model):
 
     _name='hrsystem.departments'
+    # _rec_name='name'
     deptname=fields.Char(string="اسم القسم",help="long desc",index=True)
     
