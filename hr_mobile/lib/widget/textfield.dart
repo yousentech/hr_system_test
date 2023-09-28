d0e8f9b20dc3ce7ab5aab8b3c9e0a27ecf8e505e
rrawann <rawan.sb@yahoo.com>
2023-09-28 15:35:50 +0300

    fix all transactions

--- hr_mobile/lib/widget/textfield.dart
+++ hr_mobile/lib/widget/textfield.dart
@@ -1,44 +1,45 @@
 import 'package:flutter/material.dart';
 
-class cstom_textfiled extends StatefulWidget {
-    TextEditingController textcontroller =new TextEditingController() ;
-  cstom_textfiled({required this.textcontroller});
-  @override
-  State<cstom_textfiled> createState() => _cstom_textfiledState();
-}
+class ContainerTextField extends StatelessWidget {
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final String labelText;
+  final String hintText;
+  final bool obscureText;
+  final TextEditingController controller;
+  const ContainerTextField({
+    super.key,
+    this.prefixIcon,
+    required this.labelText,
+    required this.hintText,
+    this.obscureText = false,
+    this.suffixIcon,
+    required this.controller,
+  });
 
-class _cstom_textfiledState extends State<cstom_textfiled> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 70,
-      width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.only(top: 20,right: 5,left: 5,bottom: 5),
-      decoration: BoxDecoration(
-        border: Border.all(
-                color: const Color.fromARGB(255, 242, 241, 241),
-                width: 2.0,
-              ),
-        borderRadius: BorderRadius.circular(25),
-        boxShadow: [
-                BoxShadow(
-                  color: Color.fromARGB(255, 154, 57, 204),
-                  spreadRadius: 2,
-                  blurRadius: 3,
-                  offset: Offset(0, 7), // changes the shadow position
-                ),
-              ],
-    ),
-      padding: EdgeInsets.only(right:150),
-      child: TextField(
-            controller: widget.textcontroller,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintStyle: TextStyle(fontSize: 20,fontWeight:FontWeight.bold,color: Colors.white),
-              hintText: 'اضافه موظف',
-            ),
-        )
+      margin: const EdgeInsets.all(20),
+      child: TextFormField(
+        controller: controller,
+        keyboardType: TextInputType.text,
+        obscureText: obscureText,
+        textAlignVertical: TextAlignVertical.bottom,
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          border: const UnderlineInputBorder(),
+          labelText: labelText,
+          hintText: hintText,
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return "${labelText}Requied Field";
+          }
+          return null;
+        },
+      ),
     );
   }
-
 }
