3af143f80a5910f42a06ab4cfcfd245d3f3232a3
sana-mohammed <sanamohammed71@gmail.com>
2023-10-01 11:32:11 +0300

    some changes in loan and security

--- model/loan.py
+++ model/loan.py
@@ -1,5 +1,5 @@
 from odoo import api,models,fields
-from datetime import date
+import datetime
 from odoo.exceptions import ValidationError
 
 class loan(models.Model):
@@ -8,12 +8,29 @@ class loan(models.Model):
        _rec_name='employee_id'
 
        employee_id=fields.Many2one('hr.employee',domain=[('emplo_checkbox','=','True')],string="اسم الموظف" ,required=True)
-       date=fields.Date(string="تاريخ السلفة ")
        amount=fields.Float(string="مبلغ السلفة ")
        state= fields.Selection([('not_posted','غير مرحل'),('posted','مرحل')],default="not_posted",readonly=True) 
        journal_id=fields.Many2one( 'account.journal',domain=[('type','in',['cash','bank'])],string="دفتر اليومية")
        payment_id=fields.Many2one('account.payment',string='رقم الدفعة')
+       @api.model
+       def _get_month_selection(self):
+        months = [(str(i), datetime.date(1900, i, 1).strftime('%B')) for i in range(1, 13)]
+        return months
        
+       month = fields.Selection(selection='_get_month_selection', string='الشهر', index=True)
+       def confirm(self):
+          self.write({'state': 'posted'})
+          
+       def unconfirm(self):
+              self.write({'state': 'not_posted'})
+       
+       @api.model
+       def _get_year_selection(self):
+          current_year = datetime.datetime.now().year
+          years = [(str(i), str(i)) for i in range(current_year - 10, current_year + 1)]
+          return years
+
+       year = fields.Selection(selection='_get_year_selection', string='السنة', index=True)
        def unlink(self):
            
               if self.state == 'posted':
@@ -21,11 +38,6 @@ class loan(models.Model):
          
               return super(loan,self).unlink()
  
-       @api.constrains('date')
-       def _check_date_end(self):
-        for record in self:
-         if record.date > fields.Date.today():
-            raise ValidationError("التاريخ لا يجب ان يكون في المستقبل")
           
        def get_payment (self):
             print("***************",self.payment_id.id)
@@ -40,7 +52,7 @@ class loan(models.Model):
         } 
        def confirm(self):
             payment = self.env['account.payment'].create({
-                'date': self.date,
+                'date': datetime.datetime.now(),
                 'payment_type':'outbound',
                 'partner_id':self.employee_id.partner_id.id,
                 'amount': self.amount,
