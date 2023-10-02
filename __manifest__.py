996e56d48e78a689c7a9df62864cc8498d3c04b0
rrawann <rawan.sb@yahoo.com>
2023-10-02 15:50:29 +0300

    edit loan in transaction

--- __manifest__.py
+++ __manifest__.py
@@ -11,48 +11,47 @@
     'website': "https://www.qimamhd.com",
     'company': 'yousen Techno Solutions',
     'maintainer': 'yousen Techno Solutions',
-    'depends': ['base','account','hr','stock','report_xlsx'],
-    'data': [   
-                
-                'security/security.xml',
-                'views/loan.xml',
-                'views/offdays.xml',
-                # 'views/transaction.xml',
-                'views/transaction.xml',
-               
+    'depends': ['base', 'account', 'hr', 'stock', 'report_xlsx'],
+    'data': [
+
+        'security/security.xml',
+        'views/loan.xml',
+        'views/offdays.xml',
+        'views/transaction.xml',
+
                'views/employee.xml',
                'views/departments.xml',
                'views/products.xml',
-            
+
                'report_xml/employee_report.xml',
-                'report_xml/one_employee_report.xml',
-                'report_xml/wizard.xml',
-                'views/menus.xml',
+        'report_xml/one_employee_report.xml',
+        'report_xml/wizard.xml',
+        'views/menus.xml',
               'security/ir.model.access.csv',
-         ],
-# 'assets':{
-#     'web.report_assets_common':[
-#           'static/src/fonts/K2FhfZBRmr9vQ1pHEey6GIGo8_pv3myYjuXwe55ijDz-oQ.woff2',
-#     ]
-# },
-     
-# 'qweb': [
-#     'static/src/fonts/myfont.woff2',
-# ],
-#  'css': [
-#         'static/src/css/style.css',
-#     ],
-'images':[
-     'static/src/img/code.png',
-    'static/src/img/hero-bg.png',
- 'static/src/img/hero-img.png', 
- 'static/src/img/team-shape.png',
-   
-],
+    ],
+    # 'assets':{
+    #     'web.report_assets_common':[
+    #           'static/src/fonts/K2FhfZBRmr9vQ1pHEey6GIGo8_pv3myYjuXwe55ijDz-oQ.woff2',
+    #     ]
+    # },
+
+    # 'qweb': [
+    #     'static/src/fonts/myfont.woff2',
+    # ],
+    #  'css': [
+    #         'static/src/css/style.css',
+    #     ],
+    'images': [
+        'static/src/img/code.png',
+        'static/src/img/hero-bg.png',
+        'static/src/img/hero-img.png',
+        'static/src/img/team-shape.png',
+
+    ],
 
     'license': 'LGPL-3',
     'images': [],
-    'sequence':'-100',
+    'sequence': '-100',
     'installable': True,
     'auto_install': False,
     'application': True,
